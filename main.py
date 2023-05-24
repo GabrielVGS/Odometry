e8defb760295464fe804368aae064da8f0d079aa
GabrielVGS <75582525+GabrielVGS@users.noreply.github.com>
2023-05-24 18:32:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,20 @@ from ev3dev2.button import Button
 from ev3dev2.sensor import *
 from ev3dev2.sensor.lego import *
 from ev3dev2.sensor.virtual import *
-from odometrium import *
+from odometry import Odometry
 from utils import *
 
 from time import sleep
 
 
 robot = Robot(OUTPUT_A,OUTPUT_B)
+pos = Odometry(OUTPUT_A,OUTPUT_B,5.6,15.2,360,360)
 robot._gyro  = GyroSensor(INPUT_3)
 cont = 0
 pen = Pen(INPUT_5)
 pen.down()
 while True:
-    print(f"{robot.pos_info.x}, {robot.pos_info.y}")
+    pos.log_pos()
     direction = 90 * cont
     robot.move(75,direction,35)
     sleep(0.5)
